e6374b6bee04f7d50af034fe78cea2c52ac18b37
Mathieu BROEKAERT <mbroekaert@gmail.com>
2022-08-26 20:44:38 +0200

    Integration of PagedList nugget

--- Website/Controllers/ToDoItemsController.cs
+++ Website/Controllers/ToDoItemsController.cs
@@ -1,18 +1,34 @@
 ﻿using Application.Common.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using PagedList;
 
 namespace Website.Controllers
 {
     public class ToDoItemsController : Controller
     {
         private readonly ITodoItemsService toDoItemsService;
+        
         public ToDoItemsController(ITodoItemsService toDoItemsService)
         {
             this.toDoItemsService = toDoItemsService;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            return View(await toDoItemsService.GetTodosAsync());
+            ViewBag.CurrentSort = sortOrder;
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+            int pageSize = 15;
+            int pageNumber = (page ?? 1);
+
+            return View((await toDoItemsService.GetTodosAsync()).ToPagedList(pageNumber, pageSize));
         }
     }
 }
